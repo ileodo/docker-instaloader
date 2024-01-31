9819b38fa1d373694cbef5c50d8dae1979f6574b
Tianwei Dong <ileodo@users.noreply.github.com>
2024-01-31 02:06:02 +0000

    Update run.sh

--- root/app/run.sh
+++ root/app/run.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 args="$(cat /config/args | xargs)"
-targets="$(cat /config/targets | xargs)"
-
 
 echo $(date)
-echo $args
-echo $targets
 echo "Start"
-(cd /downloads && /lsiopy/bin/instaloader +/config/args $targets)
+(cd /downloads && /lsiopy/bin/instaloader +/config/args)
 echo "End"
